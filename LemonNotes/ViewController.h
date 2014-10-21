7b2b4027bd7c9b1616ae2d205a89f63b3a8d9d06
Christopher Fu <chrisf1337@gmail.com>
2014-10-21 01:03:15 -0400

    Adding files

---
+++ LemonNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LemonNotes
+//
+//  Created by Christopher Fu on 10/21/14.
+//  Copyright (c) 2014 2 AM Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LemonNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LemonNotes
+//
+//  Created by Christopher Fu on 10/21/14.
+//  Copyright (c) 2014 2 AM Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
