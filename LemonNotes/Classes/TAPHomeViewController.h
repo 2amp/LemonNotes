1fabe7d6359fe2c64a56e44e84c01cda8929f082
Bohui Moon <moondennis@naver.com>
2014-11-08 13:59:55 -0800

    Swtiched homVC to subclass of tableVC & storyboard accordingly

--- LemonNotes/Classes/TAPHomeViewController.h
+++ LemonNotes/Classes/TAPHomeViewController.h
@@ -15,7 +15,7 @@
  * @author Bohui Moon
  * @version 0.1
  */
-@interface TAPHomeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface TAPHomeViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
 
 - (IBAction)matchesTapped:(id)sender;
 
