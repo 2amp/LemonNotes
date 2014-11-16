d8d4ac3250cd60dab133845aab8e0958561afa06
Bohui Moon <moondennis@naver.com>
2014-11-16 01:37:11 -0800

    Start of major changes
    DataManager no longer implements PickerDataSource.
    
    Synchronous addition to NSURLSession (from 3rd party)
    changed to instance methods instead of class methods.
    Instead of sharedSession, now just uses self,
    whichever that session type may be.
    
    New class SummonerManager (each SummoenrVC should have a SummonerManager).
    Slowly should implement recent matches instead of DataManager.
    DataManager should focus on static data.

---
+++ LemonNotes/Classes/Utils/SummonerManager.h
@@ -0,0 +1,13 @@
+//
+//  SummonerManager.h
+//  LemonNotes
+//
+//  Created by Bohui Moon on 11/15/14.
+//  Copyright (c) 2014 2AM Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SummonerManager : NSObject
+
+@end
---
+++ LemonNotes/Classes/Utils/SummonerManager.m
@@ -0,0 +1,13 @@
+//
+//  SummonerManager.m
+//  LemonNotes
+//
+//  Created by Bohui Moon on 11/15/14.
+//  Copyright (c) 2014 2AM Productions. All rights reserved.
+//
+
+#import "SummonerManager.h"
+
+@implementation SummonerManager
+
+@end
