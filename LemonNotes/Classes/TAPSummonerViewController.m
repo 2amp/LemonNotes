89b9c63dccdabd1f52717eff5864a05648f02ad0
Bohui Moon <moondennis@naver.com>
2014-11-24 20:23:03 -0800

    Temp made footer activity indicator spin on reaching last cell.
    TODO: make it so that more cells are loaded.
         (maybe make it wait like 0.5s so that cells aren't loaded instantly)

--- LemonNotes/Classes/TAPSummonerViewController.m
+++ LemonNotes/Classes/TAPSummonerViewController.m
@@ -18,6 +18,7 @@
 
 //Header
 @property (nonatomic) BOOL needsUpdate;
+@property (nonatomic, weak) IBOutlet UIView*  header;
 @property (nonatomic, weak) IBOutlet UILabel* summonerNameLabel;
 @property (nonatomic, weak) IBOutlet UILabel* summonerLevelLabel;
 @property (nonatomic, weak) IBOutlet UIImageView* summonerIconView;
@@ -27,8 +28,12 @@
 @property (nonatomic, strong) NSMutableArray* matches;
 @property (nonatomic, strong) UIRefreshControl* refreshControl;
 
+//footer
+@property (nonatomic, weak) IBOutlet UIView* footer;
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView* footerIndicator;
+
 //setup
-- (void)setupHeader;
+- (void)setupHeaderFooter;
 - (void)setupTableView;
 
 @end
@@ -94,13 +99,13 @@
 }
 
 /**
- * @method setupHeader
+ * @method setupHeaderFooter
  *
  * Sets up the header components.
  * @note Assumes that summonerManager has been setup
  *       and that matches array is not empty.
  */
-- (void)setupHeader
+- (void)setupHeaderFooter
 {
     //setup basic header elems
     self.summonerNameLabel.text  = self.summonerInfo[@"name"];
@@ -121,6 +126,10 @@
     
     //landscape: put header behind table view
     [self.tableView sendSubviewToBack:[self.tableView tableHeaderView]];
+    
+    
+    //footer
+    self.footer.hidden = YES;
 }
 
 /**
@@ -158,7 +167,7 @@
     
     if (self.needsUpdate)
     {
-        [self setupHeader];
+        [self setupHeaderFooter];
         self.needsUpdate = NO;
     }
     [self.tableView reloadData];
@@ -353,12 +362,12 @@
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     NSLog(@"cell will appear: %i", (int)indexPath.row);
-//    if (indexPath.row == self.matches.count - 1)
-//    {
-//        [self.manager loadMatches];
-//    }
+    if (indexPath.row == self.matches.count - 1)
+    {
+        [self.tableView tableFooterView].hidden = NO;
+        [self.footerIndicator startAnimating];
+    }
 }
-
 
 
 #pragma mark - Navigation Events
