dd3617b01442754195bb2f0bf2dccc55d55b2996
Bohui Moon <moondennis@naver.com>
2014-12-25 12:28:50 +0900

    Initial push for ScrollNavBar
    Now scrolls as expected.
    Still needs more touching.

--- LemonNotes/Classes/UI Elements/TAPScrollNavBarController.m
+++ LemonNotes/Classes/UI Elements/TAPScrollNavBarController.m
@@ -1,10 +1,13 @@
 
 #import "TAPScrollNavBarController.h"
+#import <objc/runtime.h>
 
 
 @interface TAPScrollNavBarController()
 {
-    
+    CGFloat startingY;
+    CGFloat startingOffset;
+    CGFloat previousOffset;
 }
 
 @property (nonatomic, weak) UINavigationBar *navbar;
@@ -27,6 +30,8 @@
     if (self = [super init])
     {
         self.navbar = navbar;
+        startingY = navbar.frame.origin.y;
+        startingOffset = -CGRectGetMaxY(navbar.frame);
     }
     return self;
 }
@@ -41,7 +46,9 @@
  */
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
 {
+    previousOffset = scrollView.contentOffset.y;
     
+    NSLog(@"y: %f, height: %f", self.navbar.frame.origin.y, self.navbar.frame.size.height);
 }
 
 /**
@@ -52,7 +59,14 @@
  */
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
+    CGFloat currentOffset = scrollView.contentOffset.y;
+    CGFloat delta = previousOffset - currentOffset;
+    previousOffset = currentOffset;
     
+    if (currentOffset > startingOffset && [self validDelta:delta])
+    {
+        [self scrollNavbarWithDelta:delta];
+    }
 }
 
 /**
@@ -69,6 +83,34 @@
 
 
 #pragma mark - NavBar Control
+/**
+ * @method scrollNavbarWithDelta:
+ *
+ * Given a delta value which the user scrolled,
+ * scrolls the navbar by the same delta if possible.
+ * 
+ * @param  delta value of user's scroll
+ */
+- (void)scrollNavbarWithDelta:(CGFloat)delta
+{
+    CGRect frame = self.navbar.frame;
+    frame.origin.y += delta;
+    self.navbar.frame = frame;
+}
 
+
+#pragma mark - Helper Methods
+/**
+ * @method validDelta
+ *
+ * Determines whether navbar can move the given delta
+ */
+- (BOOL)validDelta:(CGFloat)delta
+{
+    CGRect frame = self.navbar.frame;
+    CGFloat dest = frame.origin.y + delta;
+    
+    return (startingY - frame.size.height < dest) && (dest < startingY);
+}
 
 @end
