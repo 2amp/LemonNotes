39d84b0fecbd254e940e7bb0c9c26a2a852354f3
Bohui Moon <moondennis@naver.com>
2015-01-22 15:57:37 -0800

    Added docs for LemonRefresh

--- LemonNotes/Classes/UI Elements/TAPLemonRefreshControl.m
+++ LemonNotes/Classes/UI Elements/TAPLemonRefreshControl.m
@@ -84,9 +84,27 @@ typedef enum {
 
 
 #pragma mark - Accessors
-
+/**
+ * @method isRefreshing
+ *
+ * Getter to check whether current refreshing or not.
+ * @return YES if state is refreshing; otherwise, NO.
+ */
+- (BOOL)isRefreshing
+{
+    return state == kStateRefreshing;
+}
 
 #pragma mark - Scroll Control
+/**
+ * @method scrollViewWillBeginDragging:
+ *
+ * Called when user has touched the screen is about to drag.
+ * Store the starting inset for later, and adjust the size/pos
+ * of this frame and the lemon image view accordingly.
+ *
+ * @param scrollView scrollView that will begin dragging
+ */
 - (void)scrollViewWillBegingDragging:(UIScrollView *)scrollView
 {
     touchEnded = NO;
@@ -101,6 +119,22 @@ typedef enum {
     self.imageView.frame = CGRectMake(centerX, IMAGE_MARGIN, IMAGE_SIDE, IMAGE_SIDE);
 }
 
+/**
+ * @method scrollViewDidScroll:
+ *
+ * Called when user scrolls the screen by dragging.
+ * The difference between the starting inset and current offset is measured.
+ * Adjustments and animations are only made when diff > 0,
+ * meaning that user is purposely scrolling up to refresh.
+ * 
+ * If not already refreshing, change the lemon icon according to base value.
+ * When idle and lemon completes, immediately begin refreshing.
+ *
+ * When resetting after updating and diff is reverted back to 0,
+ * change state to idle for next update and assure image is back to upright.
+ *
+ * @param scrollView that is scrolling
+ */
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     CGFloat currentOffset = -scrollView.contentOffset.y;
@@ -131,6 +165,13 @@ typedef enum {
     }
 }
 
+/**
+ * @method scrollViewDidEndDragging:
+ *
+ * Called when user lifts finger from the screen.
+ * Only if diff > 0, adjust the scrollview inset
+ * to the stored inset (set by beging/end refreshing).
+ */
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView
 {
     touchEnded = YES;
@@ -140,18 +181,42 @@ typedef enum {
 
 
 #pragma mark - Refresh Control
+/**
+ * @method begingRefreshing
+ *
+ * Called when lemon is completed as user drags far enough.
+ * Switch the state to refreshing, and start spinning.
+ * Setting the EventValueChanged flag will trigger any
+ * methods set by the class using this refresh control.
+ *
+ * Stored inset is set as starting inset + control height,
+ * preventing refresh control from disappearing when touch is released.
+ *
+ * @note Only executed when state is idle to prevent multiple firing of events.
+ */
 - (void)beginRefreshing
 {
     if (state == kStateIdle)
     {
-        state = kStateRefreshing;
-    
         [self spin];
-        storedInset = startingInset + REFRESH_CONTROL_HEIGHT;
+    
+        state = kStateRefreshing;
         [self sendActionsForControlEvents:UIControlEventValueChanged];
+        
+        storedInset = startingInset + REFRESH_CONTROL_HEIGHT;
     }
 }
 
+/**
+ * @method endRefreshing
+ *
+ * Called when updating is over and refresh control should hide.
+ * Switch the state to resetting (becomes idle when fully hidden)
+ * and revert inset back to starting inset by storing it.
+ * If touch has already ended, revert to old inset immediately.
+ *
+ * @note Only executed when state is refreshing to prevent unwanted endings
+ */
 - (void)endRefreshing
 {
     if (state == kStateRefreshing)
@@ -166,6 +231,12 @@ typedef enum {
 
 
 #pragma mark - Animations
+/**
+ * @method spin
+ *
+ * Recursively spins the lemon until refreshing is over.
+ * using UIView animations and CGAffineTransform rotations.
+ */
 - (void)spin
 {
     [UIView animateWithDuration:0.4 delay:0.0 options:kNilOptions
@@ -182,6 +253,17 @@ typedef enum {
 
 
 #pragma mark - Helper
+/**
+ * @method getLemonPartWith:basedOff:
+ *
+ * Given an offset and base line value,
+ * calculates the lemon part to show accordingly with a floor func.
+ * This allows the lemon to complete exactly when offset hits base.
+ *
+ * @param offset - current variable value
+ * @param base   - value to calculate from
+ * @return lemon part to show
+ */
 - (int)getLemonPartWith:(CGFloat)offset basedOff:(CGFloat)base
 {
     CGFloat interval = base/(self.lemonParts.count-1);
@@ -189,6 +271,13 @@ typedef enum {
     return (int)MIN(part, self.lemonParts.count-1);
 }
 
+/**
+ * @method adjustScrollViewInset
+ *
+ * Changes the inset of the scrollView to the stored value.
+ * This makes the scrollView smoothly scroll to the new value,
+ * because changing the inset changes the offset of the view.
+ */
 - (void)adjustScrollViewInset
 {
     [UIView animateWithDuration:0.4
@@ -196,7 +285,7 @@ typedef enum {
     {
         UIEdgeInsets insets = self.scrollView.contentInset;
         insets.top = storedInset;
-        self.scrollView.contentInset = insets;
+        self.scrollView.contentInset  = insets;
     }];
 }
 
