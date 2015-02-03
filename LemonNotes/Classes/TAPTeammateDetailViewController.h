7ca95b1bc4a9ee2cf685d8efa0d0cfc62c87c36d
Christopher Fu <chrisf1337@gmail.com>
2015-02-02 23:14:25 -0500

    Added preliminary placeholder bar plot

--- LemonNotes/Classes/TAPTeammateDetailViewController.h
+++ LemonNotes/Classes/TAPTeammateDetailViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <CorePlot/CorePlot-CocoaTouch.h>
 
-@interface TAPTeammateDetailViewController : UIViewController <CPTPlotDataSource>
+@interface TAPTeammateDetailViewController : UIViewController <CPTBarPlotDataSource, CPTBarPlotDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *champLabel;
 @property (weak, nonatomic) IBOutlet CPTGraphHostingView *hostView;
--- LemonNotes/Classes/TAPTeammateDetailViewController.m
+++ LemonNotes/Classes/TAPTeammateDetailViewController.m
@@ -12,6 +12,9 @@ const int STATUS_BAR_HEIGHT = 20;
 const int NAVIGATION_BAR_HEIGHT = 44;
 const int TAB_BAR_HEIGHT = 49;
 
+const CGFloat BAR_WIDTH = 0.25f;
+const CGFloat BAR_INITIAL_X = 0.25f;
+
 @interface TAPTeammateDetailViewController ()
 
 @end
@@ -21,17 +24,7 @@ const int TAB_BAR_HEIGHT = 49;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
-    self.hostView.hostedGraph = graph;
-    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
-
-    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(-4)
-                                                    length:CPTDecimalFromFloat(8)];
-    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(0)
-                                                    length:CPTDecimalFromFloat(16)];
-    CPTScatterPlot *plot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
-    plot.dataSource = self;
-    [graph addPlot:plot toPlotSpace:graph.defaultPlotSpace];
+    [self initPlot];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,22 +32,104 @@ const int TAB_BAR_HEIGHT = 49;
     // Dispose of any resources that can be recreated.
 }
 
+- (void)initPlot
+{
+    self.hostView.allowPinchScaling = NO;
+    [self configureGraph];
+    [self configurePlots];
+    [self configureAxes];
+}
+
+- (void)configureGraph
+{
+    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+    graph.plotAreaFrame.masksToBorder = NO;
+    self.hostView.hostedGraph = graph;
+    [graph applyTheme:[CPTTheme themeNamed:kCPTSlateTheme]];
+    graph.paddingBottom = 30.0f;
+    graph.paddingLeft = 30.0f;
+    graph.paddingTop = -1.0f;
+    graph.paddingRight = -5.0f;
+    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
+    titleStyle.color = [CPTColor whiteColor];
+    titleStyle.fontName = @"Helvetica-Bold";
+    titleStyle.fontSize = 16.0f;
+    NSString *title = @"Portfolio Prices";
+    graph.title = title;
+    graph.titleTextStyle = titleStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, -16.0f);
+    CGFloat xMin = 0.0f;
+    CGFloat xMax = 5.0f;
+    CGFloat yMin = 0.0f;
+    CGFloat yMax = 800.0f;
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
+    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(xMin) length:CPTDecimalFromFloat(xMax)];
+    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(yMin) length:CPTDecimalFromFloat(yMax)];
+}
+
+- (void)configurePlots
+{
+    CPTBarPlot *barPlot = [CPTBarPlot tubularBarPlotWithColor:[CPTColor redColor] horizontalBars:NO];
+    barPlot.identifier = @"AAPL";
+    CPTMutableLineStyle *barLineStyle = [[CPTMutableLineStyle alloc] init];
+    barLineStyle.lineColor = [CPTColor lightGrayColor];
+    barLineStyle.lineWidth = 0.5;
+    CPTGraph *graph = self.hostView.hostedGraph;
+    CGFloat barX = BAR_INITIAL_X;
+    barPlot.dataSource = self;
+    barPlot.delegate = self;
+    barPlot.barWidth = CPTDecimalFromDouble(BAR_WIDTH);
+    barPlot.barOffset = CPTDecimalFromDouble(barX);
+    barPlot.lineStyle = barLineStyle;
+    [graph addPlot:barPlot toPlotSpace:graph.defaultPlotSpace];
+}
+
+- (void)configureAxes
+{
+    CPTMutableTextStyle *axisTitleStyle = [CPTMutableTextStyle textStyle];
+    axisTitleStyle.color = [CPTColor whiteColor];
+    axisTitleStyle.fontName = @"Helvetica-Bold";
+    axisTitleStyle.fontSize = 12.0f;
+    CPTMutableLineStyle *axisLineStyle = [CPTMutableLineStyle lineStyle];
+    axisLineStyle.lineWidth = 2.0f;
+    axisLineStyle.lineColor = [[CPTColor whiteColor] colorWithAlphaComponent:1];
+    // 2 - Get the graph's axis set
+    CPTXYAxisSet *axisSet = (CPTXYAxisSet *) self.hostView.hostedGraph.axisSet;
+    // 3 - Configure the x-axis
+    axisSet.xAxis.labelingPolicy = CPTAxisLabelingPolicyNone;
+    axisSet.xAxis.title = @"Days of Week (Mon - Fri)";
+    axisSet.xAxis.titleTextStyle = axisTitleStyle;
+    axisSet.xAxis.titleOffset = 10.0f;
+    axisSet.xAxis.axisLineStyle = axisLineStyle;
+    // 4 - Configure the y-axis
+    axisSet.yAxis.labelingPolicy = CPTAxisLabelingPolicyNone;
+    axisSet.yAxis.title = @"Price";
+    axisSet.yAxis.titleTextStyle = axisTitleStyle;
+    axisSet.yAxis.titleOffset = 5.0f;
+    axisSet.yAxis.axisLineStyle = axisLineStyle;
+}
+
 - (NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
 {
-    return 9;
+    return 5;
 }
 
 - (NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)idx
 {
-    int x = (int)idx - 4;
-    if (fieldEnum == CPTScatterPlotFieldX)
+    if (fieldEnum == CPTBarPlotFieldBarTip)
     {
-        return [NSNumber numberWithInt:x];
+        return @400;
     }
     else
     {
-        return [NSNumber numberWithInt:(x * x)];
+        return [NSDecimalNumber numberWithUnsignedInteger:idx];
     }
+}
+
+- (void)barPlot:(CPTBarPlot *)plot barWasSelectedAtRecordIndex:(NSUInteger)idx
+{
+
 }
 
 /*
