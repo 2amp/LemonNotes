f96a89db12cd4558f0c700633ae974b5645ab762
Bohui Moon <moondennis@naver.com>
2015-02-10 11:26:16 -0800

    More changes
    - header now relative positioning
    - resizing window resizes header accordingly
    - navbar now underlines orange on hover
    - found and added smooth scrolling for link clicks

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,16 +1,40 @@
 $( document ).ready(function(){
     
-    /**
-     * setup
-     * 
-     * Executed 2s after window loads
-     * Adds classes to certain elements to trigger animations
-     */
+    /* Constants */
+    var nav_height = 80;
+        
+    //inital setup
     window.onload = function()
     {
+        //header height
+        $("#header").height(window.innerHeight - nav_height);
+        
+        //start animations
         $("#title_wrap").addClass("underline-center-out-pre");
         setTimeout( function(){ $("#title_wrap").addClass("underline-center-out-post"); }, 500);
-        setTimeout( function(){ $("#phrase").addClass("fadein"); }, 800);
-       // setTimeout( function(){ $("#test_wrap").css("opacity", 1); }, 800);
+        setTimeout( function(){ $("#phrase").addClass("fadein"); }, 1200);
     }
+    
+    //resize event
+    window.onresize = function()
+    {   
+        //header
+        $("#header").height(window.innerHeight - nav_height);
+    }
+    
+    //click scroll, http://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $('a[href*=#]:not([href=#])').click(function() 
+    {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+         || location.hostname == this.hostname) 
+        {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) 
+            {
+                $('html,body').animate({ scrollTop: target.offset().top}, 850);
+                return false;
+            }
+        }
+    });
 });
